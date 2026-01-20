9b9960fc088171a8b2540fb2dcf9a77a16763a52
Abdi Deresa <abdishegute@gmail.com>
2026-01-19 19:11:38 -0800

    created a rendering function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 //use localeStorage to keep track of the tasks
 //using array to store the tasks
-const tasks=JSON.parse(localStorage.getItem("tasks")) || [];
+const tasks=JSON.parse(localStorage.getItem("tasks")) || [{text:"cook",
+  completed:false
+}];
 
 //get the elements
 const addEl=document.getElementById("add");
 const inputEl=document.getElementById("input");
 const resultEl=document.getElementById("result");
+
+function render(){
+  if(!tasks.length) return;
+
+  //reset the result to empty
+  resultEl.innerHTML="";
+
+  tasks.forEach((task,index)=>{
+    //create a list element for each task
+    const li=document.createElement('li');
+
+    li.classList.add("list-items");
+
+    li.innerHTML=`<span class="list-items ${task.completed?'list-item-selected':''}" click='${toggle(index)}'>${task.text}</span>
+                                <button class='btn-primary' click='${dele(index)}'>Delete</button>`;
+
+    //add it to the ul
+    resultEl.appendChild(li);
+  
+  });
+
+}
