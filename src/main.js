b85b3b3b5024649003c4e941a38ab240b30e09bf
Abdi Deresa <abdishegute@gmail.com>
2026-01-19 19:25:07 -0800

    completed the js portion

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ const addEl=document.getElementById("add");
 const inputEl=document.getElementById("input");
 const resultEl=document.getElementById("result");
 
+
+//add event listener
+addEl.addEl('click',addTask);
+inputEl.addEl('click',(e)=>{
+  if(e.key==="Enter") addTask();
+});
+
 //rendering function
 function render(){
   if(!tasks.length) return;
@@ -23,7 +30,7 @@ function render(){
     li.classList.add("list-items");
 
     li.innerHTML=`<span class="list-items ${task.completed?'list-item-selected':''}" click='${toggle(index)}'>${task.text}</span>
-                                <button class='btn-primary' click='${dele(index)}'>Delete</button>`;
+                                <button class='btn-primary' click='${delTask(index)}'>Delete</button>`;
 
     //add it to the ul
     resultEl.appendChild(li);
@@ -32,7 +39,31 @@ function render(){
 
 }
 
-function save(){
+function saveTask(){
   localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+function addTask(){
+  let text=inputEl.value.trim();
+  if(!text) return;
+  const task={text,
+    completed:false
+  };
+
+  tasks.push(task);
+  saveTask();
+  render();
+}
+
+function toggle(index){
+  tasks[index].completed=!completed;
+  saveTask();
+  render();
+}
+
+function delTask(index){
+  tasks.splice(index,1);
+  saveTask();
+  render();
 }
 
