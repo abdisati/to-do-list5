04d5991df51359357adf9060e03a50a639b25a75
Abdi Deresa <abdishegute@gmail.com>
2026-01-21 18:45:28 -0800

    fixed errors

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 //use localeStorage to keep track of the tasks
 //using array to store the tasks
-const tasks=JSON.parse(localStorage.getItem("tasks")) || [{text:"cook",
-  completed:false
-}];
+const tasks=JSON.parse(localStorage.getItem("tasks")) || [];
 
 //get the elements
 const addEl=document.getElementById("add");
@@ -11,17 +9,33 @@ const resultEl=document.getElementById("result");
 
 
 //add event listener
-addEl.addEl('click',addTask);
-inputEl.addEl('click',(e)=>{
+addEl.addEventListener('click',addTask);
+inputEl.addEventListener('keydown',(e)=>{
   if(e.key==="Enter") addTask();
+});
+
+resultEl.addEventListener('click',(e)=>{
+   const target = e.target.closest('[data-index]');
+  if(!target) return; 
+  const index=Number(target.dataset.index);
+
+  if(e.target.tagName==='SPAN'){
+    // call toggle
+    toggle(Number(index)); //convert it to number i.e dataset is in string
+  }
+
+  if(e.target.tagName==='BUTTON'){
+    delTask(Number(index));
+  }
 });
 
 //rendering function
 function render(){
+   resultEl.innerHTML="";
   if(!tasks.length) return;
 
-  //reset the result to empty
-  resultEl.innerHTML="";
+
+
 
   tasks.forEach((task,index)=>{
     //create a list element for each task
@@ -29,8 +43,8 @@ function render(){
 
     li.classList.add("list-items");
 
-    li.innerHTML=`<span class="list-items ${task.completed?'list-item-selected':''}" click='${toggle(index)}'>${task.text}</span>
-                                <button class='btn-primary' click='${delTask(index)}'>Delete</button>`;
+    li.innerHTML=`<span class="list-items ${task.completed?'list-item-selected':''}" data-index="${index}">${task.text}</span>
+                                <button class='btn-primary' data-index="${index}" >Delete</button>`;
 
     //add it to the ul
     resultEl.appendChild(li);
@@ -53,10 +67,13 @@ function addTask(){
   tasks.push(task);
   saveTask();
   render();
+  inputEl.value="";
 }
 
 function toggle(index){
-  tasks[index].completed=!completed;
+  console.log(index);
+  console.log(tasks[index]);
+  tasks[index].completed=!tasks[index].completed;
   saveTask();
   render();
 }
@@ -66,4 +83,6 @@ function delTask(index){
   saveTask();
   render();
 }
+
+
 
